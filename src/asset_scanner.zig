3bdde6889c312912d2c553023098bba515247444
Jack Parsons <jackpar1113@gmail.com>
2026-03-30 08:47:16 -0600

    only grab file types that we care about

--- src/asset_scanner.zig
+++ src/asset_scanner.zig
@@ -2,10 +2,12 @@ const std = @import("std");
 
 pub const Extension = enum {
     gltf,
+    other,
 
     pub fn string(self: Extension) []const u8 {
         return switch (self) {
             .gltf => "gltf",
+            .other => "other",
         };
     }
 };
@@ -20,6 +22,8 @@ pub const SourceFile = struct {
     size: u64,
 };
 
+pub const SourceFileList = std.ArrayList(SourceFile);
+
 pub const AssetScanner = struct {
     io: std.Io,
     allocator: std.mem.Allocator,
@@ -33,26 +37,42 @@ pub const AssetScanner = struct {
         };
     }
 
-    pub fn scan(self: AssetScanner) !std.ArrayList(SourceFile) {
+    pub fn scan(self: AssetScanner) !SourceFileList {
         var files: std.ArrayList(SourceFile) = .empty;
         try self.scanDir(self.dir, &files);
         return files;
     }
 
-    fn scanDir(self: AssetScanner, dir: std.Io.Dir, files: *std.ArrayList(SourceFile)) !void {
+    fn scanDir(self: AssetScanner, dir: std.Io.Dir, files: *SourceFileList) !void {
         var iter = dir.iterate();
         while (try iter.next(self.io)) |entry| {
             if (entry.kind == .file) {
-                try files.append(self.allocator, .{
-                    .extension = Extension.gltf,
-                    .path = entry.name,
-                    .size = 0,
-                });
-                std.log.info("Found {s} file: {s}", .{ Extension.gltf.string(), entry.name });
+                const ext = processEntry(entry);
+                if (ext != .other) {
+                    try files.append(self.allocator, .{
+                        .extension = ext,
+                        .path = entry.name,
+                        .size = 0,
+                    });
+                    std.log.info("Found {s} file: {s}", .{ Extension.gltf.string(), entry.name });
+                }
             } else if (entry.kind == .directory) {
                 const subdir = try std.Io.Dir.openDir(dir, self.io, entry.name, .{ .iterate = true });
                 try self.scanDir(subdir, files);
             }
         }
+    }
+
+    fn processEntry(entry: std.Io.Dir.Entry) Extension {
+        var iter = std.mem.splitScalar(u8, entry.name, '.');
+        // ignore filename itself
+        _ = iter.next();
+
+        if (iter.next()) |ext| {
+            if (map.get(ext)) |ex| {
+                return ex;
+            }
+        }
+        return .other;
     }
 };
